18656492952703cf4e574edf37876cc08c9a649f
Harsh Tuwar <tuwarharsh08@gmail.com>
2021-09-11 14:56:33 -0300

    ready for coding...all things works now

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
@@ -19,21 +18,12 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-console.log(process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
 InitRoutes(app);
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.get('/', (req, res) => {
-// 		app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// 	});
-// }
-
 app.listen(PORT, () => {
-	console.log(`We're live in ${process.env.NODE_ENV} mode on port ${PORT};`);
+	console.log(`We're live in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
