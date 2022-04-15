40771cf06f1f66bc5e6e525b74283947b0a2cab9
Harsh Tuwar <tuwarharsh08@gmail.com>
2022-04-15 19:28:43 -0400

    implemented design for signup, login and forgot password page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Box>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/forgot-password">
+            <a>Forgot Password</a>
+          </Link>
+        </li>
+      </ul>
+    </Box>
   )
 }
 
