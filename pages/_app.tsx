c6e52c234ef39db55522fc58ae7481739a2df94d
Harsh Tuwar <tuwarharsh08@gmail.com>
2022-04-15 19:00:21 -0400

    added chakraui dep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,27 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { store } from '../redux/store';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
