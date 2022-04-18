8dbc6099b7891afcf04f526bcc1badfeab968c60
Harsh Tuwar <tuwarharsh08@gmail.com>
2022-04-18 16:33:21 -0400

    landing implemented

--- constants/index.ts
+++ constants/index.ts
@@ -1,23 +1,19 @@
 import {
 	FiHome,
-	FiTrendingUp,
-	FiCompass,
-	FiStar,
 	FiSettings,
 } from 'react-icons/fi';
 import { IconType } from 'react-icons';
+import APP_ROUTES from '../utils/routes';
 
 interface LinkItemProps {
 	name: string;
 	icon: IconType;
+	route: string;
 }
 
 export const APP_NAME = "MyHotel";
 
 export const SIDEBAR_LINKS: Array<LinkItemProps> = [
-	{ name: 'Home', icon: FiHome },
-	{ name: 'Trending', icon: FiTrendingUp },
-	{ name: 'Explore', icon: FiCompass },
-	{ name: 'Favourites', icon: FiStar },
-	{ name: 'Settings', icon: FiSettings },
+	{ name: 'Home', icon: FiHome, route: APP_ROUTES.HOME },
+	{ name: 'Settings', icon: FiSettings, route: APP_ROUTES.SETTINGS },
 ];
--- utils/routes.ts
+++ utils/routes.ts
@@ -2,6 +2,7 @@ abstract class APP_ROUTES {
 	static HOME = '/';
 	static LOGIN = '/login';
 	static FORGOT_PASSWORD = '/forgot-password';
+	static SETTINGS = '/settings';
 };
 
 export default APP_ROUTES;
