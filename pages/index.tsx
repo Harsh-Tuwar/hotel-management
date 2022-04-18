769fa546276a7c60466cc8f7f79f8c39c2e0f20d
Harsh Tuwar <tuwarharsh08@gmail.com>
2022-04-17 23:24:07 -0400

    added header and sidebar for landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { Box, Button } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Link from 'next/link';
 import { AuthContext } from '../context/authContext';
 import Router from 'next/router';
 import APP_ROUTES from '../utils/routes';
+import SidebarWithHeader from '../components/layout-components/Navbar';
 
 const Home: NextPage = () => {
   const authContext = React.useContext(AuthContext);
@@ -16,28 +15,7 @@ const Home: NextPage = () => {
   }, [authContext.user]);
 
   return (
-    <Box>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/login">
-            <a>Login</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/forgot-password">
-            <a>Forgot Password</a>
-          </Link>
-        </li>
-        <li>
-          <Button colorScheme='blue' onClick={authContext.logout}>Logout</Button>
-        </li>
-      </ul>
-    </Box>
+      <SidebarWithHeader children={<div>Test</div>} />
   )
 }
 
