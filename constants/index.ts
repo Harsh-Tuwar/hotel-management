ca1df3632b0d0fdd7394ebba8d79a52b2bb0c437
Harsh Tuwar <tuwarharsh08@gmail.com>
2022-04-18 16:28:58 -0400

    Refactoring for layout components

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,23 @@
+import {
+	FiHome,
+	FiTrendingUp,
+	FiCompass,
+	FiStar,
+	FiSettings,
+} from 'react-icons/fi';
+import { IconType } from 'react-icons';
+
+interface LinkItemProps {
+	name: string;
+	icon: IconType;
+}
+
 export const APP_NAME = "MyHotel";
+
+export const SIDEBAR_LINKS: Array<LinkItemProps> = [
+	{ name: 'Home', icon: FiHome },
+	{ name: 'Trending', icon: FiTrendingUp },
+	{ name: 'Explore', icon: FiCompass },
+	{ name: 'Favourites', icon: FiStar },
+	{ name: 'Settings', icon: FiSettings },
+];
